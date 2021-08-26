cdf580f1109dce84991ab2e5b00ba088ec30b2f3
sahadan <sahadan@gmail.com>
2021-08-26 12:03:29 +0530

    first commit

---
+++ Springboot-se2-api-demo-2021/src/main/java/com/faith/app/SpringbootSe2ApiDemo2021Application.java
@@ -0,0 +1,15 @@
+package com.faith.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSe2ApiDemo2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSe2ApiDemo2021Application.class, args);
+		System.out.println("Welcome to Spring Boot-JPA-REST-SQL-Maven-Demo SE2");
+	
+	}
+
+}
---
+++ Springboot-se2-api-demo-2021/src/main/java/com/faith/app/rest/FunRestController.java
@@ -0,0 +1,29 @@
+package com.faith.app.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	// expose "/" to get response
+	@GetMapping("/")
+	public String sayHello() {
+		return " Hello Guys!" + coachName + "  Time on server is " + LocalDateTime.now();
+	}
+	
+	//@RequestMapping("/")
+	public String index() {
+		return "Greetings from Faith...:: CI/CD Integration";
+	}
+	
+	
+
+}
